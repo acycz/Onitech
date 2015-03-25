02428a034cfc497250eb79d1a0fb19e6e78c443b
Olle Niklasson <olle.niklasson@gmail.com>
2015-03-25 21:30:14 +0100

    Added Onitech.Math

---
+++ Onitech.NuGet/Onitech.Math/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onitech.Math
+{
+    public static class Calculate
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b +2;
+        }
+    }
+}
